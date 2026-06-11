02064735f100e908d71c1535bae89c4ed101f05a
roman3857 <romanshakhrai6@gmail.com>
2026-06-11 21:39:17 +0200

    commit

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-const form = document.querySelector("form")
+const form = document.querySelector("form");
 const input = document.querySelector("input[type='radio']");
 
 function backgroundChanger(event) {
-       document.body.style.backgroundColor = event.target.value;
+  document.body.style.backgroundColor = event.target.value;
 }
-form.addEventListener("change", backgroundChanger)
+form.addEventListener("change", backgroundChanger);
+
+const input2 = document.querySelector("#name-input");
+const output = document.querySelector("#name-output");
+
+input2.addEventListener("input", () => {
+       output.textContent = input2.value.trim() || "незнайомець"
+})
